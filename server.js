36ef8aa0dfafc8e17cddd47501df4b60bc75c0b8
zlChurrOsCH <40037040+zlChurrOsCH@users.noreply.github.com>
2025-01-21 19:02:41 -0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from './src/app.js'
-
-const PORT = process.env.PORT || 5000
-
-// ouve a PORTA 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT} !`)
-})
